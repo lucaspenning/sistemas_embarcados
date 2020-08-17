4f3e8e0a21340daada4e9a1987150da5422ab2f6
lucaspenning <50622975+lucaspenning@users.noreply.github.com>
2020-08-17 14:15:00 -0300

    Rename Threads.py to Multiplas_Threads.py

---
+++ src/Multiplas_Threads.py
@@ -0,0 +1,30 @@
+## Nome: Pisca_Thread.py
+## Autor: Victor Hugo Lima
+## Data: 13/05/2018
+## Finalidade: Esté programa faz piscar dois leds em tempos diferentes através de threads.
+
+import _thread
+import utime
+import machine
+pin21 = machine.Pin(21, machine.Pin.OUT)
+pin19 = machine.Pin(19, machine.Pin.OUT)
+ 
+def pisca1():
+	while True:
+		pin21.value(1)
+		utime.sleep_ms(500)
+		pin21.value(0)
+		utime.sleep_ms(500)
+def pisca2():
+	while True:
+
+		pin19.value(1)
+		utime.sleep_ms(50)
+
+		pin19.value(0)
+
+		utime.sleep_ms(50)
+
+_thread.start_new_thread(pisca1, ())
+
+_thread.start_new_thread(pisca2, ())
